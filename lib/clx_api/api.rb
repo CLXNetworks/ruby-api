15c3824b1d805c83303ce71d5a6f1e7ebd539634
Andreas Fridlund <afrxx09@student.lnu.se>
2015-05-25 12:37:18 +0200

    API gem version 0.1.0 complete, 100% coverage and documentation.

--- lib/clx_api/api.rb
+++ lib/clx_api/api.rb
@@ -37,7 +37,7 @@ module CLX
 
     # Get all operators from the API
     # @return [hash] operators
-    #   list of operators in hash.
+    #   list of operators as hash.
     # @example
     #   clx_api = CLX::API.new 'my_username', 'my_password'
     #   operators = clx_api.get_operators
@@ -47,19 +47,85 @@ module CLX
     end
 
     # Get one operator based on it's operator id
-    # @param [Integer] id Operator ID
+    # @param [Integer] operator_id Operator ID
     # @return [hash] operator
-    #   One operator hash.
+    #   One operator as hash.
     # @example
     #   clx_api = CLX::API.new 'my_username', 'my_password'
     #   operator = clx_api.get_operator(1)
-    def get_operator_by_id(id)
-      raise CLXException, 'Operator id must be integer' unless id.is_a? Integer
-      raise CLXException, 'Operator id must be greater than zero' unless id > 0
-      
-      url = CLX.paths[:operator] + '/' + id.to_s
+    def get_operator_by_id(operator_id)
+      valid_id?(operator_id)
+      url = CLX.paths[:operator] + '/' + operator_id.to_s
       @http_client.get(url)
     end
+
+    # Get all gateways
+    # @return [hash] gateways
+    #   list of gateways as hash.
+    def get_gateways
+      url = CLX.paths[:gateway]
+      @http_client.get(url)
+    end
+    
+    # Get one gateway based on it's gateway id
+    # @param [Integer] gateway_id Gateway ID
+    # @return [hash] gateway
+    #   One operators as hash.
+    def get_gateway_by_id(gateway_id)
+      valid_id?(gateway_id)
+      url = CLX.paths[:gateway] + '/' + gateway_id.to_s
+      @http_client.get(url)
+    end
+    
+    # Get price entires based on gateway id
+    # @param [Integer] gateway_id Gateway ID
+    # @return [hash] price_entires
+    #   list of price entries as hash.
+    def get_price_entires_by_gateway_id(gateway_id)
+      valid_id?(gateway_id)
+      url = CLX.paths[:gateway] + '/' + gateway_id.to_s + '/price'
+      @http_client.get(url)
+    end
+
+    # Get price entiry based on gateway id and operator id
+    # @param [Integer] gateway_id Gateway ID
+    # @param [Integer] operator_id Operator ID
+    # @return [hash] price_entry
+    #   one price entry as hash.
+    def get_price_entries_by_gateway_id_and_operator_id(gateway_id, operator_id)
+      valid_id?(gateway_id)
+      valid_id?(operator_id)
+      url = CLX.paths[:gateway] + '/' + gateway_id.to_s + '/price/' + operator_id.to_s
+      @http_client.get(url)
+    end
+
+    # Get price entiry based on gateway id, operator id and a date
+    # @param [Integer] gateway_id Gateway ID
+    # @param [Integer] operator_id Operator ID
+    # @param [DateTime] date DateTime
+    # @return [hash] price_entry
+    #   one price entry as hash.
+    def get_price_entries_by_gateway_id_and_operator_id_and_date(gateway_id, operator_id, date)
+      valid_id?(gateway_id)
+      valid_id?(operator_id)
+      valid_date?(date)
+      date_query = "?date=#{date.year}-#{date.month}-#{date.day}"
+      url = CLX.paths[:gateway] + '/' + gateway_id.to_s + '/price/' + operator_id.to_s + '/' + date_query
+      @http_client.get(url)
+    end
+
+    private
+
+      # Validates that in is of type integer and is grater than zero
+      def valid_id?(id)
+        raise CLXException, 'Id must be integer' unless id.is_a? Integer
+        raise CLXException, 'Id must be greater than zero' unless id > 0
+      end
+
+      #Validates that date is of type DateTime
+      def valid_date?(date)
+        raise CLXException, 'Date must be of type DateTime' unless date.is_a? DateTime
+      end
 
   end
 
--- test/api_test.rb
+++ test/api_test.rb
@@ -74,7 +74,20 @@ class APITest < MiniTest::Test
     assert_equal client.base_url, 'http://new.url'
   end
 
-  # Get Operators test
+  def test_request_with_invalid_username_and_password_raises_CLX_exception
+    @api.set_auth('wrong_username', 'wrong_password')
+    response = OpenStruct.new(
+      body: '{}',
+      code: 401
+    )
+    @api.http_client.http_adapter.response = response
+
+    assert_raises CLX::CLXAPIException do
+      result = @api.get_operators
+    end
+  end
+
+  #Operator tests
   def test_get_operators_returns_list_of_operators
     response = OpenStruct.new(
       body: '[
@@ -89,22 +102,9 @@ class APITest < MiniTest::Test
     
     refute_nil result
     refute_empty result
+    assert_equal(result.size, 2)
   end
 
-  def test_get_operators_with_invalid_username_and_password_raises_CLX_exception
-    @api.set_auth('wrong_username', 'wrong_password')
-    response = OpenStruct.new(
-      body: '{}',
-      code: 401
-    )
-    @api.http_client.http_adapter.response = response
-
-    assert_raises CLX::CLXAPIException do
-      result = @api.get_operators
-    end
-  end
-
-  #Get operator by id test
   def test_get_operator_by_id_returns_single_operator
     response = OpenStruct.new(
       body: '{"id":1,"name":"Foo","network":"Foonetwork","uniqueName":"Foo uniquq","isoCountryCode":"8","operationalState":"active","operationalStatDate":"-0001-11-30 00:00:00","numberOfSubscribers":0}',
@@ -144,6 +144,162 @@ class APITest < MiniTest::Test
     @api.http_client.http_adapter.response = response
     assert_raises CLX::CLXAPIException do
       result = @api.get_operator_by_id(1)
+    end
+  end
+
+  #Gateway tests
+  def test_that_get_gateways_return_a_collection_of_gateways
+    response = OpenStruct.new(
+      body: '[
+        {"id":1,"name":"gateway no 1","type":"Some type 1"},
+        {"id":2,"name":"gateway no 2","type":"Some type 2"}
+      ]',
+      code: 200
+    )
+    @api.http_client.http_adapter.response = response
+
+    result = @api.get_gateways
+    
+    refute_nil result
+    refute_empty result
+    assert_equal(result.size, 2)
+  end
+
+  def test_that_get_gateway_by_id_returns_single_gateway
+    response = OpenStruct.new(
+      body: '{"id":1,"name":"gateway no 1","type":"Some type 1"}',
+      code: 200
+    )
+    @api.http_client.http_adapter.response = response
+
+    gateway = @api.get_gateway_by_id(1)
+
+    assert_equal gateway.id, 1
+  end
+
+  def test_that_get_gateway_by_by_with_id_that_does_not_exist_raises_clx_api_exception
+    response = OpenStruct.new(
+      body: '{"error":{"message": "No gateway with id: 9999"}}',
+      code: 404
+    )
+    @api.http_client.http_adapter.response = response
+
+    assert_raises CLX::CLXAPIException do
+      gateway = @api.get_gateway_by_id(9999)
+    end
+  end
+
+  def test_that_get_gateway_by_id_with_non_integer_id_raises_clx_exception
+    assert_raises CLX::CLXException do
+      gateway = @api.get_gateway_by_id('a string')
+    end
+  end
+
+  def test_that_get_price_entires_by_gateway_id_return_collection_of_price_entires
+    response = OpenStruct.new(
+      body: '[
+        {"price": 0.25,"gateway":"gateway no 1","operator":"Some operator 1"},
+        {"price": 0.35,"gateway":"gateway no 1","operator":"Some operator 2"}
+      ]',
+      code: 200
+    )
+    @api.http_client.http_adapter.response = response
+
+    result = @api.get_price_entires_by_gateway_id(1)
+
+    refute_nil result
+    refute_empty result
+    assert_equal(result.size, 2)
+  end
+
+  def test_that_get_price_entires_by_gateway_id_with_id_that_does_not_exist_raises_clx_api_exception
+    response = OpenStruct.new(
+      body: '{"error":{"message": "No gateway with id: 9999"}}',
+      code: 404
+    )
+    @api.http_client.http_adapter.response = response
+
+    assert_raises CLX::CLXAPIException do
+      result = @api.get_price_entires_by_gateway_id(9999)
+    end
+  end
+
+  def test_that_get_price_entries_by_gateway_id_with_non_integer_id_raises_clx_exception
+    assert_raises CLX::CLXException do
+      gateway = @api.get_price_entires_by_gateway_id('a string')
+    end
+  end
+
+  def test_that_get_price_entry_by_gateway_id_and_operator_id_returns_price_entry
+    response = OpenStruct.new(
+      body: '{"price": "0.25","gateway": "gateway_1","operator": "Some operator","expireDate": 0}',
+      code: 200
+    )
+    @api.http_client.http_adapter.response = response
+
+    gateway = @api.get_price_entries_by_gateway_id_and_operator_id(1, 1)
+
+    assert_equal gateway.gateway, "gateway_1"
+  end
+
+  def test_that_get_price_entries_by_gateway_id_and_operator_id_with_non_integer_ids_raises_clx_exception
+    assert_raises CLX::CLXException do
+      gateway = @api.get_price_entries_by_gateway_id_and_operator_id('a string', 1)
+    end
+    assert_raises CLX::CLXException do
+      gateway = @api.get_price_entries_by_gateway_id_and_operator_id(1, 'a string')
+    end
+    assert_raises CLX::CLXException do
+      gateway = @api.get_price_entries_by_gateway_id_and_operator_id('a string', 'a string')
+    end
+  end
+
+  def test_that_get_price_entry_by_gateway_id_and_operator_id_and_date_returns_price_entry
+    response = OpenStruct.new(
+      body: '{"price": "0.25","gateway": "gateway_1","operator": "Some operator","expireDate": 0}',
+      code: 200
+    )
+    @api.http_client.http_adapter.response = response
+
+    gateway = @api.get_price_entries_by_gateway_id_and_operator_id_and_date(1, 1, DateTime.now)
+
+    assert_equal gateway.gateway, "gateway_1"
+  end
+
+  def test_that_get_price_entry_by_gateway_id_and_operator_id_and_date_with_ids_that_does_not_exist_raises_clx_api_exception
+    response = OpenStruct.new(
+      body: '{"error":{"message": "No gateway with id: 9999"}}',
+      code: 404
+    )
+    @api.http_client.http_adapter.response = response
+
+    assert_raises CLX::CLXAPIException do
+      result = @api.get_price_entries_by_gateway_id_and_operator_id_and_date(9999, 1, DateTime.now)
+    end
+
+    response = OpenStruct.new(
+      body: '{"error":{"message": "Trying to get current and next price: PriceLinkedList missing for operator with id: 9999 in pirce list with id: 1", "code": 1002}}',
+      code: 404
+    )
+    @api.http_client.http_adapter.response = response
+
+    assert_raises CLX::CLXAPIException do
+      result = @api.get_price_entries_by_gateway_id_and_operator_id_and_date(1, 9999, DateTime.now)
+    end
+  end
+
+  def test_that_get_price_entry_by_gateway_id_and_operator_id_and_date_with_invalid_parameters_raises_clx_exception
+    assert_raises CLX::CLXException do
+      gateway = @api.get_price_entries_by_gateway_id_and_operator_id_and_date('a string', 1, DateTime.now)
+    end
+    assert_raises CLX::CLXException do
+      gateway = @api.get_price_entries_by_gateway_id_and_operator_id_and_date(1, 'a string', DateTime.now)
+    end
+    assert_raises CLX::CLXException do
+      gateway = @api.get_price_entries_by_gateway_id_and_operator_id_and_date('a string', 'a string', DateTime.now)
+    end
+    assert_raises CLX::CLXException do
+      gateway = @api.get_price_entries_by_gateway_id_and_operator_id_and_date(1, 1, 'should_be_datetime')
     end
   end
 
